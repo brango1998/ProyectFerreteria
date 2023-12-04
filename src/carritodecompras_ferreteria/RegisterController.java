67944a1d2734284453fdd3c640873bfd3dd541e7
neon <neon@DESKTOP-5DO94CV.bbrouter>
2023-12-03 20:08:15 -0500

    10

--- src/carritodecompras_ferreteria/RegisterController.java
+++ src/carritodecompras_ferreteria/RegisterController.java
@@ -4,7 +4,9 @@
  */
 package carritodecompras_ferreteria;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,6 +16,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -43,10 +46,35 @@ public class RegisterController implements Initializable {
 
     @FXML
     private void save(ActionEvent event) {
+        String campo1 = v1.getText();
+        String campo2 = v2.getText();
+
+        if (!registroExiste(campo1, campo2)) {
+            guardarEnArchivo(campo1, campo2);
+            mostrarMensaje("Registro guardado correctamente.");
+        } else {
+            mostrarError("El registro ya existe.");
+        }
     }
 
     @FXML
-    private void cance(ActionEvent event) {
+    private void cance(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        openWindow("Login.fxml", stage);
+    }
+    private boolean registroExiste(String campo1, String campo2) {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] campos = line.split(","); // Suponiendo que los campos están separados por coma (puedes ajustarlo según tu estructura)
+                if (campos.length >= 2 && campos[0].equals(campo1) && campos[1].equals(campo2)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
     private void guardarEnArchivo(String campo1, String campo2) {
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(FILE_NAME, true)))) {
